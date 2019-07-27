e6d329c5af8e2673d01d06ff98395b86ef6c5a0c
songtm <565038419@qq.com>
2019-07-27 14:17:20 +0800

    auto ab: strict mode, editor下也给警告,防止正式出包的时候一堆错误

--- Assets/Plugins/XAsset/Custom/XAssets.cs
+++ Assets/Plugins/XAsset/Custom/XAssets.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Plugins.XAsset;
 #if UNITY_EDITOR
@@ -10,6 +11,7 @@ namespace XAsset.Plugins.XAsset.Custom
 {
     public static class XAssets
     {
+        private static HashSet<string> _assetSet;
         public static void Initialize(Action onSuccess, Action<string> onError)
         {
             Bundles.OverrideBaseDownloadingUrl += bundleName =>
@@ -18,7 +20,9 @@ namespace XAsset.Plugins.XAsset.Custom
                 return null;
             };
 
-            //Bundles.OverrideBundleDispater //todo bundle异步加载分发策略
+            //todo bundle异步加载分发策略 目前web bundle好像不能控制下载缓存路径什么的,要不要写一个httpBundle?
+            //Bundles.OverrideBundleDispater
+
             InitEditorAssetLoader();
 
             Assets.Initialize(onSuccess, onError);
@@ -27,9 +31,33 @@ namespace XAsset.Plugins.XAsset.Custom
         [Conditional("UNITY_EDITOR")]
         private static void InitEditorAssetLoader()
         {
+            if (!Utility.assetBundleMode)
+            {
+                _assetSet = new HashSet<string>();
+                AssetsManifest manifest = AssetDatabase.LoadAssetAtPath<AssetsManifest>(Utility.AssetsManifestAsset);
+                if (manifest == null)
+                {
+                    Debug.LogError("Pls gen manifest first:<");
+                }
+
+                string[] dirs = manifest.dirs;
+                foreach (var assetData in manifest.assets)
+                {
+                    _assetSet.Add(dirs[assetData.dir] + "/" + assetData.name);
+                }
+            }
+
             Utility.loadDelegate = (assetPath, type) =>
             {
-                if (Utility.assetBundleMode) Debug.LogError("bundle mode load with editor: " + assetPath);
+                if (Utility.assetBundleMode)
+                    Debug.LogError("bundle mode load with editor: " + assetPath);
+                else
+                {
+                    if (!_assetSet.Contains(assetPath))
+                    {
+                        Debug.LogWarning("Pls config/update manifest for " + assetPath);
+                    }
+                }
                 return AssetDatabase.LoadAssetAtPath(assetPath, type);
             };
         }
